421303e3897a58d1639b46807ef61340147b8af3
usanuman <52057554+usanuman@users.noreply.github.com>
2023-01-13 00:04:08 -0500

    Update install-docker-compose.sh
    docker-compose install latest version

--- install-docker-compose.sh
+++ install-docker-compose.sh
@@ -14,8 +14,11 @@ sudo apt-get update
 sudo apt-get -y install docker-ce
 
 # Install docker-compose
-sudo curl -L "https://github.com/docker/compose/releases/download/v2.6.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod a+x /usr/local/bin/docker-compose
+#sudo curl -L "https://github.com/docker/compose/releases/download/v2.6.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+#sudo chmod a+x /usr/local/bin/docker-compose
+curl -L https://github.com/docker/compose/releases/download/v$(curl -Ls https://www.servercow.de/docker-compose/latest.php)/docker-compose-$(uname -s)-$(uname -m) > /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+
 # Enable & start docker
 sudo systemctl enable docker
 sudo systemctl start docker
