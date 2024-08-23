8aadce7e86818a01b35fa0c30b3204d436be867d
Numan Hussain <52057554+usanuman@users.noreply.github.com>
2024-08-22 22:34:58 -0400

    Create iptables_firewall.sh

---
+++ iptables_firewall.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+# Clear IPTables rules:
+iptables -P INPUT ACCEPT
+iptables -F
+iptables -X
+
+iptables -A INPUT -i lo -j ACCEPT
+iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
+
+# Allow SSH connections from YOUR_IP 
+iptables -A INPUT -p tcp -m tcp -m state -m comment -s 0.0.0.0/0 --dport 22 --state NEW -j ACCEPT --comment "SSH"
+
+# Allow HTTP/HTTPS 
+iptables -A INPUT -p tcp -m tcp -m state -m comment -s 0.0.0.0/0 --dport 80 --state NEW -j ACCEPT --comment " HTTP "
+iptables -A INPUT -p tcp -m tcp -m state -m comment -s 0.0.0.0/0 --dport 443 --state NEW -j ACCEPT --comment " HTTPS "
+
+# Allow PING's
+iptables -A INPUT -p icmp --icmp-type echo-request -j ACCEPT
+
+iptables -P INPUT DROP
+iptables -P FORWARD DROP
+iptables -P OUTPUT ACCEPT
+
+# Save rules
+iptables-save
